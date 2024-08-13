f3ab8aefa8b882a8f1e5ddf61cbcb9a4a9bd87c1
Matta <166758235+HamaJjigae@users.noreply.github.com>
2024-08-13 00:17:10 -0600

    completes vertical and horizontal checker(dict) algorithms for use in sorting the equipment list

--- Algorithms/VerticalChecker.cs
+++ Algorithms/VerticalChecker.cs
@@ -6,68 +6,71 @@ using System.Threading.Tasks;
 
 namespace DBConnectedFinalProjectThing.Algorithms
 {
-    public static class HorizontalChecker
+    public static class VerticalChecker
     {
-        public static Dictionary<(int, int), int> CheckRowForSpaces(char[,] array, int rowIndex)
+        public static Dictionary<(int, int), int> CheckColumnForSpaces(char[,] array, int columnIndex)
         {
-            if (rowIndex < 0 || rowIndex >= array.GetLength(0))
+            if (columnIndex < 0 || columnIndex >= array.GetLength(1))
             {
-                throw new ArgumentOutOfRangeException(nameof(rowIndex), "Row index is out of range.");
+                throw new ArgumentOutOfRangeException(nameof(columnIndex), "Column index is out of range.");
             }
 
-            int rowLength = array.GetLength(1);
+            int columnLength = array.GetLength(0);
 
-            if (rowLength <= 2)
+            if (columnLength <= 2)
             {
-                throw new ArgumentException("Row length must be greater than 2 to check for spaces between indices.");
+                throw new ArgumentException("Column length must be greater than 2 to check for spaces between indices.");
             }
+
             bool prevBlock = false;
             bool minSpaceDone = false;
             int track = 0;
             int trackReal = 0;
-            int minSpace = rowLength / 20;
+            int minSpace = columnLength / 20;
             Dictionary<(int, int), int> exportDict = new Dictionary<(int, int), int>();
-            for (int i = 1; i < rowLength - 1; i++)
+
+            for (int i = 1; i < columnLength - 1; i++)
             {
-                if (i == rowLength - 2)
+                if (i == columnLength - 2)
                 {
                     if (minSpaceDone == false)
                     {
                         continue;
                     }
-                    else if (array[rowIndex, i] == ' ' && minSpaceDone && i == rowLength - 2)
+                    else if (array[i, columnIndex] == ' ' && minSpaceDone && i == columnLength - 2)
                     {
                         track = 0;
                         trackReal++;
-                        exportDict.Add((rowIndex, i - trackReal), trackReal);
+                        exportDict.Add((i - trackReal, columnIndex), trackReal);
                         trackReal = 0;
                         continue;
                     }
                 }
-                if (array[rowIndex, i] == ' ' && minSpaceDone == false && track < minSpace)
+
+                if (array[i, columnIndex] == ' ' && minSpaceDone == false && track < minSpace)
                 {
                     track++;
                     continue;
                 }
-                else if (array[rowIndex, i] == ' ' && minSpaceDone == false && track >= minSpace)
+                else if (array[i, columnIndex] == ' ' && minSpaceDone == false && track >= minSpace)
                 {
                     track++;
                     trackReal++;
                     minSpaceDone = true;
                     continue;
                 }
-                else if (array[rowIndex, i] == ' ' && minSpaceDone)
+                else if (array[i, columnIndex] == ' ' && minSpaceDone)
                 {
                     trackReal++;
                     continue;
                 }
-                else if (array[rowIndex, i] != ' ' && prevBlock)
+                else if (array[i, columnIndex] != ' ' && prevBlock)
                 {
                     continue;
                 }
-                else if (array[rowIndex, i] != ' ' && prevBlock == false)
+                else if (array[i, columnIndex] != ' ' && prevBlock == false)
                 {
-                    exportDict.Add((rowIndex, i - trackReal), trackReal);
+                    exportDict.Add((i - trackReal, columnIndex), trackReal);
                     track = 0;
                     trackReal = 0;
                     prevBlock = true;
